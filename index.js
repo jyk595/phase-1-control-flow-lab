7b053b85623ebe427208bc477d993fb84322b447
John <jyk595@gmail.com>
2021-07-08 09:42:20 -0700

    commit tree 45fd6800b5701f0b526b38b04a2245875fe008fd

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// let traveledRide = 4;
+
+// function scuberGreetingForFeet(){  
+//   if (traveledRide <= 400) {
+//     return `This one is on me!`;
+//   } else if (traveledRide > 2000) {
+//     return `I will gladly take your thirty bucks.`;
+//   } else if (traveledRide > 2500) {
+//     return `No can do.`;
+//   }
+// }
+
+// function ternaryCheckCity(city){
+//   city === 'NYC' ? `Ok, sounds good.` : `No go.`;
+
+//   return 
+// }
+
+// function switchOnCharmFromTip(){
+//   // Write your code here!
+// }
+
+
+
+function scuberGreetingForFeet(distance) {
+  if (distance > 2500) {
+    return 'No can do.';
+  } else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distance < 400) {
+    return 'This one is on me!';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(location) {
+  let phrase = location === `NYC` ? 'Ok, sounds good.' : 'No go.';
+
+  return phrase;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+const switchOnCharmFromTip = tip => {
+  switch (tip) {
+    case 'generous' :
+      return 'Thank you so much.';
+      break;
+    case 'not as generous' :
+      return 'Thank you.';
+      break;
+    default :
+      return 'Bye.';
+  }
 }
